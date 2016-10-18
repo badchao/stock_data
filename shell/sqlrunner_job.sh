dae9cba16f19e14c70babe65fe21f6d83147d862
badqiu <badqiu@gmail.com>
2016-10-18 10:48:07 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- shell/sqlrunner_job.sh
+++ shell/sqlrunner_job.sh
@@ -2,5 +2,5 @@
 
 source /etc/profile
 
-java -cp "/data/app/stock_data/stock_data-web-admin/WEB-INF/lib/*" com.github.stock_data.cron_job.SqlRunnerJob
-
+java -cp "/data/app/stock_data/stock_data-web-admin/WEB-INF/lib/*:/usr/local/tomcat/lib/*" com.github.stock_data.cron_job.SqlRunnerJob
+ 
