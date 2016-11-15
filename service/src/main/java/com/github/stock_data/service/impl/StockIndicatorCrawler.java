41ef4eb4c692b1149df7d56b03fd812126144d21
badqiu <badqiu@gmail.com>
2016-11-15 18:04:29 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- service/src/main/java/com/github/stock_data/service/impl/StockIndicatorCrawler.java
+++ service/src/main/java/com/github/stock_data/service/impl/StockIndicatorCrawler.java
@@ -4,9 +4,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.security.cert.X509Certificate;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -117,4 +128,60 @@ public class StockIndicatorCrawler {
 		}
 	}
 	
+	
+	static {
+	    disableSslVerification();
+	}
+
+	private static void disableSslVerification() {
+		try {
+			// Create a trust manager that does not validate certificate chains
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+
+				public void checkClientTrusted(X509Certificate[] certs,
+						String authType) {
+				}
+
+				public void checkServerTrusted(X509Certificate[] certs,
+						String authType) {
+				}
+
+				@Override
+				public void checkClientTrusted(
+						java.security.cert.X509Certificate[] arg0, String arg1)throws CertificateException {
+				}
+
+				@Override
+				public void checkServerTrusted(
+						java.security.cert.X509Certificate[] arg0, String arg1)
+						throws CertificateException {
+					// TODO Auto-generated method stub
+					
+				}
+			} };
+
+			// Install the all-trusting trust manager
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection
+					.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+			// Create all-trusting host name verifier
+			HostnameVerifier allHostsValid = new HostnameVerifier() {
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			};
+
+			// Install the all-trusting host verifier
+			HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		}
+	}
 }
